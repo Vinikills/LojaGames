e000519c9f79a9ccbdde6abe1b1a4296849810a0
Vinicius Fernando <viniferan@gmail.com>
2023-02-01 19:58:36 -0300

    atv

---
+++ src/main/java/com/games/model/Produto.java
@@ -0,0 +1,101 @@
+package com.games.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+
+	@Size (min = 2, max = 100)
+	public String nome;
+	
+
+	@Size (min = 2, max = 100)
+	public String plataforma;
+
+	public String ano;
+	
+
+//	@Size (min = 2, max = 100)
+	public String faixaetaria;
+	
+
+	public Double valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getFaixaetaria() {
+		return faixaetaria;
+	}
+
+	public void setFaixaetaria(String faixaetaria) {
+		this.faixaetaria = faixaetaria;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	}
---
+++ src/main/java/com/games/reporitory/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.games.reporitory;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.games.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	public List<Categoria> findAllByEstiloContainingIgnoreCase(@Param("estilo")String estilo);
+
+}
